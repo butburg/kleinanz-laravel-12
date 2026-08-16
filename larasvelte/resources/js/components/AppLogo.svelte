487f83692580f79163e125bf7c3ab0e08386aa41
butburg <fire.fm@hotmail.de>
2026-08-16 22:41:31 +0200

    style(ui): use round logo in sidebar

--- larasvelte/resources/js/components/AppLogo.svelte
+++ larasvelte/resources/js/components/AppLogo.svelte
@@ -5,9 +5,7 @@
 </script>
 
 <div class="inline-flex items-center gap-3">
-    <div class="flex aspect-square size-8 shrink-0 items-center justify-center rounded-md bg-sidebar-primary">
-        <AppLogoIcon class="size-5 fill-current text-white! dark:text-black!" />
-    </div>
+    <AppLogoIcon class="size-9 shrink-0 rounded-full object-cover" />
     <div class="flex items-center">
         <span class="truncate text-sm font-semibold">{appName}</span>
     </div>
